92957f7e58eb43257af62d97ad486d743deea2dc
alihanozdogan <alihanozdogan@eskisehir.edu.tr>
2022-03-27 04:17:34 +0300

    Ürün Ekleme Tamamlandı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kantinmanager';
+export class AppComponent implements OnInit {
+  public products: Product[];
+  public editProduct: Product;
+  public deleteProduct: Product;
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  public getProducts(): void {
+    this.productService.getAllProducts().subscribe(
+      (response: Product[]) => {
+        this.products = response;
+        
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddProduct(addForm: NgForm): void {
+    document.getElementById('add-employee-form').click();
+    this.productService.addProduct(addForm.value).subscribe(
+      (response: Product) => {
+        console.log(response);
+        this.getProducts();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateProduct(product: Product): void {
+    this.productService.updateProduct(product).subscribe(
+      (response: Product) => {
+        console.log(response);
+        this.getProducts();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteProduct(productId: number): void {
+    this.productService.deleteProduct(productId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getProducts();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchProducts(key: string): void {
+    console.log(key);
+    const results: Product[] = [];
+    for (const product of this.products) {
+      if (product.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || product.productCode.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(product);
+      }
+    }
+    this.products = results;
+    if (results.length === 0 || !key) {
+      this.getProducts();
+    }
+  }
+
+  public onOpenModal(product: Product, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addProductModal');
+    }
+    if (mode === 'edit') {
+      this.editProduct = product;
+      button.setAttribute('data-target', '#updateProductModal');
+    }
+    if (mode === 'delete') {
+      this.deleteProduct = product;
+      button.setAttribute('data-target', '#deleteProductModal');
+    }
+    container.appendChild(button);
+    button.click();
+  }
+
+
+
 }
