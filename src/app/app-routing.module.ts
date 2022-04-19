85f6851cdcd1cec1cc2107eac16d954c710609ed
Enes Yayla <enesyayla1386@gmail.com>
2022-04-19 20:33:46 +0300

    addToCart page eklendi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddToCartComponent } from './components/add-to-cart/add-to-cart.component';
 import { CategoryComponent } from './components/category/category.component';
 import { EmployeeAddComponent } from './components/employeeCRUD/employee-add/employee-add.component';
 import { HomeComponent } from './components/home/home.component';
@@ -14,18 +15,32 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: WelcomeComponent},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'navbar', component: NavbarComponent, canActivate: [AuthGuard]},
-  {path: 'login', component: LoginComponent},
-  {path: 'product', component: ProductComponent, canActivate: [AuthGuard]},
-  {path: 'product-crud', component: ProductAddComponent},
-  {path: 'employee-crud', component: EmployeeAddComponent},
-  {path: '**', component: NotFoundComponent}, 
+  { path: '', pathMatch: 'full', component: WelcomeComponent },
+  { path: }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'navbar', component: NavbarComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'product', component: ProductComponent, canActivate: [AuthGuard] },
+  {
+    path: 'product-crud',
+    component: ProductAddComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'employee-crud',
+    component: EmployeeAddComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'add-to-card',
+    component: AddToCartComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
