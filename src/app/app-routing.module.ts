7c1bbffef7d695c1133707943fe49b7cb7bc2749
Enes Yayla <enesyayla1386@gmail.com>
2022-04-26 23:19:12 +0300

    26 Nisan Deneme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { StatisticsComponent } from './components/statistics/statistics.componen
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthGuard } from './guards/auth.guard';
 
+
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: WelcomeComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
